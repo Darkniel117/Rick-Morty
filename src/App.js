4b56356ab30aa33d1ed54439db364bf62b14ce69
Daniel Velasquez <danield-117@outlook.com>
2023-06-29 21:51:09 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ImgRickmorty from "./img/rick-morty.png"
+import './Styles/App.css';
+import { useState } from "react";
+import Characters from "./Components/Characters";
 function App() {
+  const [characters, setCharacters] = useState(null);
+  const ReqApi = async() =>{
+    const Api = await fetch("https://rickandmortyapi.com/api/character/")
+    const characterApi = await Api.json();
+
+    setCharacters(characterApi.results);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">Rick & Morty</h1>
+
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters}/>
+        ) : (
+          //Fractment
+          <>
+          <img src={ImgRickmorty} alt="Rick & Morty" className="img-home"></img>
+          <button onClick={ReqApi} className="btn-search">Buscar Personaje</button>
+          </>
+        )}
       </header>
     </div>
   );
